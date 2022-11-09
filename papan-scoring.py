0ff75c8176a0fa308cc4e0fa7ffa08f7b51c0689
arifin220195 <arifinscoutpasker@gmail.com>
2022-11-09 16:36:51 +0700

    aaa

--- papan-scoring.py
+++ papan-scoring.py
@@ -6,7 +6,9 @@ while tor:
     item = str(input('masukkan item : '))
   
     if item == 'jumbo':
-        print("Tortilla Jumbo")
+        print("\n======================\n")
+        print('==  Tortilla Jumbo  ==')
+        print("\n======================\n")
         pack = 20
         keranjang = int(input('Jml Keranjang : '))
         sisa = int(input('Sisa : '))
@@ -19,7 +21,9 @@ while tor:
         print("\n======================\n") 
 
     elif item == 'besar':
-        print('Tortilla Besar')
+        print("\n======================\n")
+        print('==  Tortilla Besar  ==')
+        print("\n======================\n")
         pack = 25
         keranjang = int(input('Jml Keranjang : '))
         sisa = int(input('Sisa : '))
@@ -32,7 +36,9 @@ while tor:
         print("\n======================\n")
 
     elif item =='abd':
-        print ('Tortilla ABD')
+        print("\n======================\n")
+        print('==  Tortilla ABD  ==')
+        print("\n======================\n")
         pack = 50
         keranjang = int(input('Jml Keranjang : '))
         sisa = int(input('Sisa : '))
@@ -44,7 +50,9 @@ while tor:
         print('PLUS SORTIR : ', plus_Sortir, 'pack')
         print("\n======================\n")
     elif item == 'sedang':
-        print('Tortilla Sedang')
+        print("\n======================\n")
+        print('==  Tortilla Sedang  ==')
+        print("\n======================\n")
         pack = 25
         keranjang = int(input('Jml Keranjang : '))
         sisa = int(input('Sisa : '))
@@ -57,7 +65,9 @@ while tor:
         print("\n======================\n")
   
     elif item == 'kecil':
-        print('Tortilla Kecil')
+        print("\n======================\n")
+        print('==  Tortilla Kecil  ==')
+        print("\n======================\n")
         pack = 33
         keranjang = int(input('Jml Keranjang : '))
         sisa = int(input('Sisa : '))
@@ -69,9 +79,10 @@ while tor:
         print('PLUS SORTIR : ', plus_Sortir, 'pack')
         print("\n======================\n")
    
-    elif item== 'mini':
-
-        print('Tortilla Mini')
+    elif item == 'mini':
+        print("\n======================\n")
+        print('==  Tortilla Mini  ==')
+        print("\n======================\n")
         pack = 40
         keranjang = int(input('Jml Keranjang : '))
         sisa = int(input('Sisa : '))
@@ -84,8 +95,9 @@ while tor:
         print("\n======================\n")
 
     elif item == 'maryam':
-
-        print('Maryam')
+        print("\n======================\n")
+        print('==  Maryam  ==')
+        print("\n======================\n")
         pack = 50
         keranjang = int(input('Jml Keranjang : '))
         sisa = int(input('Sisa : '))
